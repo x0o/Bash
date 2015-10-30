4c4f6b38af1f6e846b39cf709f285693b15fc87d
b1n1x <symmetric.key@gmail.com>
2015-10-30 10:10:12 +0000

    syntax cleanup errlib.bash
    Not tested

--- libs/errlib.bash
+++ libs/errlib.bash
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 function err() { #@USAGE: err "What am i doing"
-    printf "\n%s\n" "#################################################################"
-    printf "%s" "error: Line ${BASH_LINENO[0]} in function ${FUNCNAME[1]} "
-    printf "%s\n" "which is in the file ${BASH_SOURCE[1]}"
-    printf "%s\n" "error: Message was: $@"
-    printf "%s\n\n" "#################################################################"
+    printf "\n%s\n%s%s\n%s\n%s\n\n"                                     \
+    "#################################################################" \
+    "error: Line ${BASH_LINENO[0]} in function ${FUNCNAME[1]} "         \
+    "which is in the file ${BASH_SOURCE[1]}"                            \
+    "error: Message was: $@"                                            \
+    "#################################################################"
 }
